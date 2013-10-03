dd2545efdcc910a12ad708f7f2c47038e100d273
ophis <ophis.w@gmail.com>
2013-10-02 21:02:57 -0400

    add

--- src/test/tester.java
+++ src/test/tester.java
@@ -1,10 +1,13 @@
 package test;
 
+import authentication.authen;
+
 public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		authen A = new authen();
+		A.getToken();
 	}
 
 }
