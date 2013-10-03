577aef65f5f32b68554c66403ac99e8a6f5d63b6
ophis <ophis.w@gmail.com>
2013-10-02 22:20:16 -0400

    update

--- examples/weibo4j/examples/timeline/UpdateStatus.java
+++ examples/weibo4j/examples/timeline/UpdateStatus.java
@@ -8,8 +8,8 @@ import weibo4j.model.WeiboException;
 public class UpdateStatus {
 
 	public static void main(String[] args) {
-		String access_token = args[0];
-		String statuses = args[1];
+		String access_token = new authentication.authen().getToken();
+		String statuses = "test";
 		Timeline tm = new Timeline();
 		tm.client.setToken(access_token);
 		try {
--- src/test/tester.java
+++ src/test/tester.java
@@ -1,13 +1,36 @@
 package test;
 
 import authentication.authen;
+import weibo4j.*;
+import weibo4j.model.WeiboException;
 
 public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		authen A = new authen();
-		A.getToken();
+		String token = A.getToken();
+		Users um = new Users();
+		um.client.setToken(token);
+		Friendships fm = new Friendships();
+		fm.client.setToken(token);
+			String uid = "2308247745";
+			long i = Long.parseLong(uid);
+			long startMili=System.currentTimeMillis();
+			int a=0;
+			for(int j=0;j<100;j++)
+			{
+				uid = String.valueOf(i+j);
+				try {
+					um.showUserById(uid);
+					a++;
+				} catch (WeiboException e) {
+					// TODO Auto-generated catch block
+					
+				}
+			}
+			long endMili=System.currentTimeMillis();
+			System.out.println("总耗时为："+(endMili-startMili)+"毫秒"+" hit:"+a);
 	}
 
 }
