49b2263f2d52228209f6d845b2bda3d3736196e8
Ophis <Ophis@gmail.com>
2013-10-11 17:08:46 -0400

    update

--- src/security/core/TimelineCrawler.java
+++ src/security/core/TimelineCrawler.java
@@ -5,28 +5,42 @@ import java.util.List;
 import security.authentication.Authen;
 import security.dal.TimelineDAL;
 
-
 import weibo4j.Timeline;
 import weibo4j.model.Status;
 import weibo4j.model.StatusWapper;
+import weibo4j.model.WeiboException;
 
 public class TimelineCrawler {
-	public int crawlPublic() throws Exception{
+	public int crawlPublic() throws Exception {
 		Timeline tm = new Timeline();
-    	try {
+		boolean firstround=true;
+		try {
 			tm.setToken(Authen.getAuthen().getToken());
-	    	StatusWapper sw = tm.getPublicTimeline(200,0);
-	    	List<Status> status = sw.getStatuses();
-	    	TimelineDAL td = new TimelineDAL();
-	    	td.addAll2Timeline(status);
-	    	CommentCrawler cm = new CommentCrawler();
-	    	for (Status s : status) {
-	    		if(s.getCommentsCount()>0)
-	    			cm.crawl(s.getMid());
+			while (true) {
+				StatusWapper sw = tm.getPublicTimeline(200, 0);
+				List<Status> status = sw.getStatuses();
+				TimelineDAL td = new TimelineDAL();
+				td.addAll2Timeline(status);
+				CommentCrawler cm = new CommentCrawler();
+				for (Status s : status) {
+					if (s.getCommentsCount() > 0)
+						cm.crawl(s.getMid());
+				}
+				firstround = false;
 			}
 		} catch (Exception e) {
+			if (e instanceof WeiboException) {
+				WeiboException weiboException = (WeiboException) e;
+				int errorcode = weiboException.getErrorCode();
+				if (errorcode == 10023 || errorcode == 10022) {
+					if (firstround) {
+						return crawlPublic();
+					} else {
+						return -1;
+					}
+				}
+			}
 			throw e;
 		}
-		return 0;
 	}
 }
--- src/security/drivers/UserInfoCollector.java
+++ src/security/drivers/UserInfoCollector.java
@@ -1,35 +1,89 @@
 package security.drivers;
 
 import security.authentication.Authen;
+import security.core.TimelineCrawler;
 import security.core.UserProfileCrawler;
+import weibo4j.model.WeiboException;
 
 public class UserInfoCollector {
 	public static void main(String[] args) throws InterruptedException {
-		while(true){
-			int threadCount = 8;
-			Authen.init();
-			UserProfileCrawler.init(null);
-			Thread[] threads = new Thread[threadCount];
-			for(int i=0;i<threadCount;i++){
-				threads[i]=new Thread(new Runnable() {
-					@Override
-					public void run() {
-						// TODO Auto-generated method stub
-						UserProfileCrawler uc = new UserProfileCrawler();
+		Thread userThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while (true) {
+					int threadCount = 8;
+					Authen.init();
+					UserProfileCrawler.init(null);
+					Thread[] threads = new Thread[threadCount];
+					for (int i = 0; i < threadCount; i++) {
+						threads[i] = new Thread(new Runnable() {
+							@Override
+							public void run() {
+								UserProfileCrawler uc = new UserProfileCrawler();
+								try {
+									uc.crawl();
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						});
+					}
+					for (int i = 0; i < threadCount; i++) {
+						threads[i].start();
 						try {
-							uc.crawl();
-						} catch (Exception e) {
+							threads[i].join();
+						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
-				});	
-				threads[i].start();
+					UserProfileCrawler.storeUidList();
+				}
 			}
-			for (int i = 0; i < threads.length; i++) {
-				threads[i].join();
-			}
-			UserProfileCrawler.storeUidList();
-		}
+		});
+		userThread.start();
+//		Thread timeThread = new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				while(true){
+//					Thread[] threads2 = new Thread[2];
+//					for (int i = 0; i < 2; i++) {
+//						
+//						threads2[i] = new Thread(new Runnable() {
+//							@Override
+//							public void run() {
+//								// TODO Auto-generated method stub
+//								TimelineCrawler tc = new TimelineCrawler();
+//								try {
+//									tc.crawlPublic();
+//								} catch (Exception e) {
+//									if (e instanceof WeiboException) {
+//										WeiboException weiboException = (WeiboException) e;
+//										int errorcode = weiboException.getErrorCode();
+//										if (errorcode == 10023 || errorcode == 10022) {
+//										}
+//										else {
+//											e.printStackTrace();
+//										}
+//									}
+//								}
+//							}
+//						});
+//						threads2[i].start();
+//					}
+//					try {
+//						threads2[0].join();
+//						threads2[1].join();
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		});
+//		timeThread.start();
 	}
 }
